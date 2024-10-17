4c6ca3bfacbf7a4547d176278fbe32995a8a36eb
himanshumittal168 <himanshumittal168@gmail.com>
2024-10-17 20:21:40 +0530

    signup logic

---
+++ controller/Signup.js
@@ -0,0 +1,55 @@
+const User=require("../model/User");
+
+const bcrypt=require("bcrypt");
+// 1. input name,email,pass,role
+// 2. check if user already exist
+// 3. hash the pass
+// 4. Create and save user
+
+
+
+exports.signup=async(req,resp)=>
+{
+    try
+    {
+        const {name,email,pass,role}=req.body;
+
+        const ispresent=await User.findOne({email});
+
+        if(ispresent)
+        {
+            return resp.status(400).json({
+                success:false,
+                msg:"User Already Exsits"
+            })
+        }
+        let hp;
+        try
+        {
+            hp=await bcrypt.hash(pass,10);
+        }
+        catch(err)
+        {
+            return resp.status(500).json({
+                success:false,
+                msg:"Error in Hashing Pass"
+            })
+        }
+
+        const user=await User.create(
+            { name,role,email,pass:hp}
+        )
+
+        return resp.status(200).json({
+            success:true,
+            msg:"USER CREATED"
+        })
+    }
+    catch(err)
+    {
+        resp.status(500).json({
+            success:false,
+            msg:"INTERNAL SERVER ERROR"
+        })
+    }
+}
